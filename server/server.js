5fb9cd5805852d23efad6126e7ab3abb71d5ce4c
acharyaranjit10 <159681278+acharyaranjit10@users.noreply.github.com>
2025-07-16 07:52:34 +0545

    Serve React frontend from Express

--- server/server.js
+++ server/server.js
@@ -31,4 +31,14 @@ app.get('/', (req, res) => {
 
 // Start server
 const PORT = process.env.PORT || 5000;
+
+const path = require('path');
+
+// Serve frontend build
+app.use(express.static(path.join(__dirname, '../client/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
+
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
